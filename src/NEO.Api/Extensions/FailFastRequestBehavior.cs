9d27df12bca068775cc24c59b006c27d2b1e893b
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-08-03 20:49:25 -0300

    Adjust of version of Fluent Validation

--- src/NEO.Api/Extensions/FailFastRequestBehavior.cs
+++ src/NEO.Api/Extensions/FailFastRequestBehavior.cs
@@ -20,13 +20,14 @@ namespace NEO.Api.Extensions
 
         public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
-            
+            var context = new ValidationContext<TRequest>(request);
+
 
             var failures = _validators
-                .Select(v => v.Validate(request))
-                .SelectMany(result => result.Errors)
-                .Where(f => f != null)
-                .ToList();
+          .Select(v => v.Validate(context))
+          .SelectMany(result => result.Errors)
+          .Where(f => f != null)
+          .ToList();
 
             return failures.Any()
                 ? Errors(failures)
